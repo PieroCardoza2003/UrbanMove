fc5de75f62f928b6a580dd5c3f5d8757e6f36843
PedrashoGod <pedritoberru@gmail.com>
2024-09-14 19:02:19 -0500

    H01 - update 1.1

--- app/src/main/java/com/capstone/urbanmove/presentation/ui/emaillogin/Login.kt
+++ app/src/main/java/com/capstone/urbanmove/presentation/ui/emaillogin/Login.kt
@@ -1,22 +1,11 @@
-package com.capstone.urbanmove.presentation.ui.phonelogin
+package com.capstone.urbanmove.presentation.ui.emaillogin
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.capstone.urbanmove.databinding.ActivityPhoneLoginBinding
-import com.capstone.urbanmove.presentation.ui.otpverification.OtpVerification
-import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.PhoneAuthCredential
-import com.google.firebase.auth.PhoneAuthOptions
-import com.google.firebase.auth.PhoneAuthProvider
-import com.google.firebase.auth.PhoneAuthProvider.OnVerificationStateChangedCallbacks
-import java.util.concurrent.TimeUnit
 
-class PhoneLogin : AppCompatActivity() {
+class Login : AppCompatActivity() {
     private lateinit var binding: ActivityPhoneLoginBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var verificationId: String

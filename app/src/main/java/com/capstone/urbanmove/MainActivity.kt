0708cb4f50a8d5f0dfac254cbd9ecb9618f20f7b
PedrashoGod <pedritoberru@gmail.com>
2024-09-14 19:16:29 -0500

    ayuda a shapi

--- app/src/main/java/com/capstone/urbanmove/MainActivity.kt
+++ app/src/main/java/com/capstone/urbanmove/MainActivity.kt
@@ -4,18 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.capstone.urbanmove.databinding.ActivityMainBinding
 import com.capstone.urbanmove.presentation.ui.dateinformation.activity_date
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
-        setContentView(R.layout.activity_main)
-        val intent = Intent(this, activity_date::class.java)
-        startActivity(intent)
-        finish()
-
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
